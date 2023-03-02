a24cf8235bdb739f22d84a4204e04102846ef7d6
Yann Zerlaut <yann.zerlaut@icm-institute.org>
2023-03-02 14:08:24 +0100

    adding matplotlib style

---
+++ python/matplotlib_style.py
@@ -0,0 +1,42 @@
+### FIGURE
+figure.facecolor: white
+figure.dpi: 150
+figure.figsize: 1.7, 1.3
+figure.titlesize: 8
+# figure.titleweight: bold
+figure.autolayout: False # When True, automatically adjust subplot
+
+### LINE
+lines.linewidth: 1
+
+### PATCHES
+patch.facecolor: 0072B2
+
+### LEGEND
+legend.fontsize: 8
+
+### TICKS
+xtick.direction: out
+ytick.direction: out
+
+### FONTS
+# font.family: sans-serif
+# font.sans-serif: Avenir, 'Avenir Next LT Pro', Arial, Helvetica, sans-serif
+# font.weight: bold
+font.size: 8
+
+### AXES
+axes.grid: False
+axes.facecolor: white
+axes.linewidth: 1.0
+axes.titlesize: 8
+# axes.titleweight: bold
+axes.labelsize: 8 
+# axes.labelweight: bold
+axes.spines.top: False
+axes.spines.right: False
+axes.prop_cycle: cycler('color', ['0072B2', '009E73', 'D55E00', 'CC79A7', 'F0E442', '56B4E9'])
+
+### LINES
+lines.markersize: 2.5 # marker size, in points
+
--- python/notebook.py
+++ python/notebook.py
@@ -16,11 +16,14 @@
 # %%
 import numpy as np
 import matplotlib.pylab as plt
+import matplotlib as mpl
+mpl.rcParams["figure.facecolor"]='lightgrey'
+mpl.rcParams["figure.dpi"]=200
 
 # %%
-fig, AX = plt.subplots(1, 3, figsize=(7,1.5))
+fig, AX = plt.subplots(1, 4, figsize=(7,1.5))
 plt.subplots_adjust(wspace=0.5, top=.7, left=.35)
-fig.patch.set_facecolor('tab:grey')
+# fig.patch.set_facecolor('tab:grey')
 AX[0].imshow(np.random.randn(10,10))
 AX[0].set_ylabel('y-label')
 AX[1].scatter(np.arange(100), np.random.randn(100))
