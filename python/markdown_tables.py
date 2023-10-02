8e56a7eabebd190cdb606b364dbd00a303179352
Yann Zerlaut <yann.zerlaut@gmail.com>
2023-10-02 14:26:22 +0200

    adding the possibility to put comments in table

--- python/markdown_tables.py
+++ python/markdown_tables.py
@@ -1,15 +1,3 @@
-
-EXAMPLE="""
-|                   |                   **Parameter Description**                          |       **Name**      |      **Value**       |    **Unit**     |                  _comment_                      |
-|-------------------|----------------------------------------------------------------------|---------------------|----------------------|-----------------|-------------------------------------------------|
-| **all**           |                                                                      |                     |                      |                 |                                                 |
-|                   | time constant for smoothing in the Gaussian filter                   | `Tsmoothing`        | 1.32 10<sup>-4</sup> | ms              |                                                 |
-| **Figure 1**      |                                                                      |                     |                      |                 |                                                 |
-|                   | a threshold used in Fig 1                                            | `Fig1_threshTheta`  | 18.3                 | N/A             |                                                 |
-| **Figure 2**      |                                                                      |                     |                      |                 |                                                 |
-|                   | a threshold used in Fig 2                                            | `Fig2_threshTheta`  | 32.7 10<sup>-4</sup> | N/A             |                                                 |
-"""
-
 def format_key(value):
     if '10<sup>' in value:
         return float(value.replace(' 10<sup>', 'e').replace('</sup>', ''))
@@ -30,7 +18,7 @@ def read_table(filename,
 
         for line in f.readlines():
             split = line.split('| `')
-            if len(split)>1:
+            if len(split)>1 and ('<!--|' not in line):
                 key = split[1].split('`')[0]
                 value = split[1].split('| ')[1].split(' |')[0]
                 table[key] = format_key(value)
@@ -39,10 +27,11 @@ def read_table(filename,
 
 if __name__=='__main__':
 
-    import tempfile, os
-    with open(os.path.join(tempfile.gettempdir(), 'table.md'), 'w') as f:
-        f.write(EXAMPLE)
+    # import tempfile, os
+    # with open(os.path.join(tempfile.gettempdir(), 'table.md'), 'w') as f:
+        # f.write(EXAMPLE)
 
-    table = read_table(os.path.join(tempfile.gettempdir(), 'table.md'))
+    # table = read_table(os.path.join(tempfile.gettempdir(), 'table.md'))
+    table = read_table('README.md')
     print(table)
 
