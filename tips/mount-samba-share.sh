972eea6b29eb6dfc5944e3d53c18e216ec009312
Yann Zerlaut <yann.zerlaut@gmail.com>
2024-10-02 17:29:50 +0200

    add samba client instructions

--- tips/mount-samba-share.sh
+++ tips/mount-samba-share.sh
@@ -1,7 +1,13 @@
 # mount the samba share
 gio mount smb://iss/rebola
-# you can get info on the "local path" with: gio info smb://iss/rebola
+
 # then refresh the UI
 killall gfvsd
 nemo -q
 nemo
+
+# you can get info on the "local path" with: 
+gio info smb://iss/rebola
+
+# otherwise
+smbclient -U yann.zerlaut //iss/rebola
